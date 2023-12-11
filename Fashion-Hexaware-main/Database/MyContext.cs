01a59e486f7c559b5396718d62d7839cd02c3fd3
meghana3339 <69587463+meghana3339@users.noreply.github.com>
2023-12-11 16:17:22 +0530

    commit

---
+++ Fashion-Hexaware-main/Database/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using FashionHexa.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FashionHexa.Database
+{
+    public class MyContext : DbContext
+    {
+        private IConfiguration config;
+        public MyContext(IConfiguration cfg) 
+        {
+            config = cfg;
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Availability> Availabilities { get; set; }
+        public DbSet<UserRatings> UserRatings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP-DH150A5\\MSSQLSERVER01; Initial Catalog =projDb; User Id = meghana; Password = meghana3339; TrustServerCertificate = true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
